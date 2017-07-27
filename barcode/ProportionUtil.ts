3da9ccbc31eebb3904a4e83bfda26d91edc4bcff
Curtis Sapp <curtmsapp@gmail.com>
2017-07-27 13:44:04 -0700

    ProportionUtil is now ready to calculate coordinates
    This is the pixel variation, soon to the logo details will be change to percentages

--- barcode/ProportionUtil.ts
+++ barcode/ProportionUtil.ts
@@ -6,75 +6,100 @@ export class ProportionUtil {
     private logo: ILogoDetail;
 
     /*this.logo = {
-     LOGOWIDTH: 380,
-     LOGOHEIGHT: 450,
+     width: 380,
+     height: 450,
      BARWIDTH: 10,
      STRIPHEIGHT: 40,
      BARSPERSTRIP: 24,
      }*/
 
-    constructor(topCorner: { x: number, y: number }, bottomCorner: { x: number, y: number }, sideCorner: { x: number, y: number }) {
-        // Order is this way because the bottom corner will always be below side corner
-        let widthCalcTriangleWidth = sideCorner.x - bottomCorner.x;
-        let widthCalcTriangleHeight = sideCorner.y - bottomCorner.y;
+    // topCorner is as a person looking at the image would say is the top
+    constructor(private topCorner: IPoint, private bottomCorner: IPoint, private sideCorner: IPoint) {
+        // Order is this way because the bottom corner will always be greater than side
+        let widthCalcTriangleWidth = bottomCorner.x - sideCorner.x;
+        let widthCalcTriangleHeight = bottomCorner.y - sideCorner.y;
 
-        // Order is reversed because the top corner will always be above the side corner
+        // Order is different because the top corner will always be get in x but less than y to the side corner
         let heightCalcTriangleWidth = topCorner.x - sideCorner.x;
-        let heightCalcTriangleHeight = topCorner.y - sideCorner.y;
+        let heightCalcTriangleHeight = sideCorner.y - topCorner.y;
 
         this.logo = {
-            LOGOWIDTH: this.getHypotenuseLength(widthCalcTriangleWidth,widthCalcTriangleHeight),
-            LOGOHEIGHT: this.getHypotenuseLength(heightCalcTriangleWidth,heightCalcTriangleHeight),
+            width: this.getHypotenuseLength(widthCalcTriangleWidth, widthCalcTriangleHeight),
+            height: this.getHypotenuseLength(heightCalcTriangleWidth, heightCalcTriangleHeight),
             BARWIDTH: 10,
             STRIPHEIGHT: 40,
             BARSPERSTRIP: 24,
+            radiansRotated: Math.acos(widthCalcTriangleHeight / widthCalcTriangleWidth),
         }
     }
 
 
-    public getStartingPoint(): { x: number, y: number } {
-        let x = this.getCornerWidth() + (this.getShortBarWidth() * this.logo.BARSPERSTRIP);
-        let y = this.getCornerHeight() / 2;
+    public getStartingPoint(): IPoint {
+        // x and y are inverted accross their respective lengths because the calculation are from the bottom right corner but the orgin is in the top left
+        let x = this.logo.width - (this.getCornerWidth() + (this.getShortBarWidth() * this.logo.BARSPERSTRIP));
+        let y = this.logo.height - (this.getCornerHeight() / 2);
 
-        return {x: x, y: y};
+        return this.getImageCoordFromLogoCoord({x, y});
     }
 
-    private getShortBarHeight(): number {
-        return this.logo.BARWIDTH / this.logo.LOGOHEIGHT;
+    public getHalfWayPoint(): IPoint {
+        let x = this.getShortBarWidth() * this.logo.BARSPERSTRIP;
+        let y = this.getCornerWidth() + (this.getShortBarWidth() * this.logo.BARSPERSTRIP) - (this.getCornerHeight() / 2);
+
+        return this.getImageCoordFromLogoCoord({x, y});
     }
 
-    private getShortBarWidth(): number {
-        return this.logo.STRIPHEIGHT / this.logo.LOGOWIDTH;
+    public getShortBarHeight(): number {
+        return this.logo.BARWIDTH / this.logo.height * 100;
     }
 
-    private getTallBarHeight(): number {
-        return this.logo.STRIPHEIGHT / this.logo.LOGOHEIGHT;
+    public getShortBarWidth(): number {
+        return this.logo.STRIPHEIGHT / this.logo.width * 100;
     }
 
-    private getTallBarWidth(): number {
-        return this.logo.BARWIDTH / this.logo.LOGOWIDTH;
+    public getTallBarHeight(): number {
+        return this.logo.STRIPHEIGHT / this.logo.height * 100;
     }
 
-    private getCornerHeight(): number {
-        return this.logo.STRIPHEIGHT / this.logo.LOGOHEIGHT;
+    public getTallBarWidth(): number {
+        return this.logo.BARWIDTH / this.logo.width * 100;
     }
 
-    private getCornerWidth(): number {
-        return this.logo.STRIPHEIGHT / this.logo.LOGOWIDTH;
+    public getCornerHeight(): number {
+        return this.logo.STRIPHEIGHT / this.logo.height * 100;
     }
 
-    private getHypotenuseLength(a:number, b:number) {
+    public getCornerWidth(): number {
+        return this.logo.STRIPHEIGHT / this.logo.width * 100;
+    }
+
+    private getHypotenuseLength(a: number, b: number): number {
         // Pythagorean theorem
         return Math.sqrt((a * a) + (b * b));
+    }
+
+
+    private getImageCoordFromLogoCoord(logoCoord: IPoint): IPoint {
+        // Formula from https://stackoverflow.com/questions/20104611/find-new-coordinates-of-a-point-after-rotation
+        // This is for counter clockwise rotation, see source first comment
+        let x = (-logoCoord.y * Math.sin(this.logo.radiansRotated)) + (logoCoord.x * Math.cos(this.logo.radiansRotated));
+        let y = (logoCoord.y * Math.cos(this.logo.radiansRotated)) + (logoCoord.x * Math.sin(this.logo.radiansRotated));
+        return {x: x, y: y};
     }
 
 
 }
 
 export interface ILogoDetail {
-    LOGOWIDTH: number,
-    LOGOHEIGHT: number,
+    width: number,
+    height: number,
     BARWIDTH: number,
     STRIPHEIGHT: number,
-    BARSPERSTRIP: number
+    BARSPERSTRIP: number,
+    radiansRotated: number,
+}
+
+export interface IPoint {
+    x: number,
+    y: number
 }
